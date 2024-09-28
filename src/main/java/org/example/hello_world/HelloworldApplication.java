0649907adeda984be654faa1ab52c50499723173
Viktoriia Volosozhar <vvolosozhar1@gmail.com>
2024-09-28 22:04:19 +0300

    hello world 2

--- src/main/java/org/example/hello_world/HelloworldApplication.java
+++ src/main/java/org/example/hello_world/HelloworldApplication.java
@@ -1,4 +1,4 @@
-package org.example.hello_world.HelloworldApplication;
+package org.example.hello_world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
